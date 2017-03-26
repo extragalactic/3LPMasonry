de6b6fc8af24f9c8d3eda78444526b9bbfd7f2ef
Jonathon Fritz <jfritz50@gmail.com>
2017-03-26 14:40:12 -0700

    customer handbook update (#45)
    * test update
    
    * get finished survey query
    
    * add logo to repo
    
    * Completed most of the PDF text edits, and did basic page formatting
    
    * updated input for createPDF mutation
    
    * added section conditional logic, page breaks, more formatting
    
    * Added site photos section
    
    * new batch of generics descriptions
    
    * merge conflict fix
    
    * merge conflict fix
    
    * send estimate function complete
    
    * fix conflict
    
    * delete prices added
    
    * survey local image saved to db
    
    * update
    
    * customer upload app added to project
    
    * updates to customer upload app
    
    * updates to customer upload app
    
    * Update npm-start.sh
    
    * Update npm-start.sh
    
    * added redirect to react router, customer upload
    
    * updates to customer handbook
    
    * updates to customer handbook
    
    * updates to customer handbook

--- server/ssr/customerupload/src/App.js
+++ server/ssr/customerupload/src/App.js
@@ -4,7 +4,6 @@ import MobileDetect from 'mobile-detect';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { Router, browserHistory } from 'react-router';
-import routes from './routes';
 import { addSurveyPhoto, toggleSurveyReady } from './graphql/mutations';
 import './App.css';
 import './carousel.css';
