976fe150e84c09e8c470e309e1e818098a1fe9d7
Jonathon Fritz <jfritz50@gmail.com>
2017-03-26 06:26:02 -0700

    Update npm-start.sh

--- npm-start.sh
+++ npm-start.sh
@@ -1,3 +1,7 @@
 cd /var/app
+sudo pm2 stop --interpreter babel-node server/server.js --watch
 sudo npm install babel-cli sharp webpack nodemon
+sudo npm install
+sudo webpack
+sudo pm2 start --interpreter babel-node server/server.js --watch
 
