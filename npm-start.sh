968350909bc94a628d044e6ba0d781af555a75ba
Jonathon Fritz <jfritz50@gmail.com>
2017-03-26 18:09:48 -0700

    Update npm-start.sh

--- npm-start.sh
+++ npm-start.sh
@@ -3,5 +3,8 @@ sudo pm2 stop --interpreter babel-node server/server.js --watch
 sudo npm install babel-cli sharp webpack nodemon
 sudo npm install
 sudo webpack
+cd server/ssr/customerupload
+sudo yarn
+sudo yarn build
 sudo pm2 start --interpreter babel-node server/server.js --watch
 
