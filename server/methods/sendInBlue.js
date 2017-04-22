89ed3b8ac24d6e8b8461a6ab328168d326aca48b
Jonathon Fritz <jfritz50@gmail.com>
2017-04-21 17:30:06 -0700

    fix pdf spacing (#96)
    * update
    
    * add delete notes mutation, changes to estimate data structure:
    
    * update servercode
    
    * update::
    
    * update
    
    * update:
    
    * update delete price
    
    * edit pricing:wq
    
    * update
    
    * updates to pricing
    
    * moved autocomplete query client side, bypassed redux forms for address
    
    * search customers mutation added
    
    * mutation added to get single photo
    
    * fixed spacing for address on estimate PDF

--- server/methods/sendInBlue.js
+++ server/methods/sendInBlue.js
@@ -14,7 +14,7 @@ const sendEmailEstimatetoCustomer = (customer) => {
   require('../../node_modules/mailin-api-node-js/V2.0/mailin');
   const client = new Mailin('https://api.sendinblue.com/v2.0', process.env.MAILIN, 5000);
   const emailData = { id: 3,
-    to: customer.email1,
+    to: customer.email1 ? customer.email1 : customer.email2,
     cc: 'j@jonathonfritz.com',
     attr: { CUSTOMER: customer.firstName, LINK: `3lpm.s3.ca-central-1.amazonaws.com/${customer._id}/${customer.firstName}${customer.lastName}Estimate.pdf` },
   };
