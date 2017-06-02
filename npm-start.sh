720103d4c7b8393e8c33d5c531390d159862eb6f
Jonathon Fritz <jfritz50@gmail.com>
2017-06-02 16:35:10 -0700

    Update npm-start.sh

--- npm-start.sh
+++ npm-start.sh
@@ -1,7 +1,8 @@
 cd /var/app
-sudo pm2 stop --interpreter babel-node server/server.js 
-sudo npm install babel-cli sharp webpack nodemon canvas
-sudo yarn install --ignore-engines 
+sudo pm2 stop --interpreter babel-node server/server.js
+sudo rm -rf node_modules
+sudo yarn install babel-cli sharp webpack nodemon canvas --ignore-engines
+sudo yarn --ignore-engines
 sudo webpack
 cd customerupload
 sudo yarn
