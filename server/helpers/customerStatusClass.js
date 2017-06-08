42bfd9d513c70952be51ad8252d1fe7d2dc3cbdc
john fritz <johnfritz@johns-Mac-mini.local>
2017-06-08 11:02:52 -0700

    update status

--- server/helpers/customerStatusClass.js
+++ server/helpers/customerStatusClass.js
@@ -2,12 +2,60 @@ import _ from 'lodash';
 import CustomersModel from '../lib/CustomerModel';
 import UsersModel from '../lib/UserModel';
 import QueueModel from '../lib/queueModel';
+import OneSignalClass from './oneSignalClass';
 
 class CustomerStatus {
   constructor(customer, user) {
     this.customer = customer;
     this.user = user;
   }
+
+  getCustomer() {
+    const customerObj = new Promise((resolve, reject) => {
+      CustomersModel.findOne({ _id: this.customer })
+          .then(customer => resolve(customer));
+    });
+    return customerObj;
+  }
+
+  getUser() {
+    const userObj = new Promise((resolve, reject) => {
+      UsersModel.findOne({ _id: this.customer })
+          .then(customer => resolve(customer));
+    });
+    return userObj;
+  }
+
+ getEstimateStatus(status) {
+   if(status === 0) {
+     return 5;
+   }
+    if (status === 1) {
+      return 6;
+    }
+    if (status === 2) {
+      return 7;
+    }
+    if (status === 3) {
+      return 8;
+    }
+  }
+
+  getSurveyor() {
+    const surveyor = new Promise((resolve, reject) => {
+      UsersModel.findOne({ _id: this.user })
+       .then((user) => {
+         resolve({
+           firstName: user.firstName,
+           lastName: user.lastName,
+           mobile: user.mobile,
+           id: user._id,
+         });
+       });
+    });
+    return surveyor;
+  }
+
   setEstimatorName() {
     UsersModel.findOne({ _id: this.user })
      .then((user) => {
@@ -18,13 +66,8 @@ class CustomerStatus {
           });
      });
   }
-  getCustomer() {
-    const customerObj = new Promise((resolve, reject) => {
-      CustomersModel.findOne({ _id: this.customer })
-          .then(customer => resolve(customer));
-    });
-    return customerObj;
-  }
+
+
   addCustomertoUserList() {
     UsersModel.findOne({ _id: this.user })
         .then((user) => {
@@ -45,6 +88,17 @@ class CustomerStatus {
                user.save();
              });
         });
+  }
+  removeCustomerfromEstimate() {
+    this.getCustomer.then((customer) => {
+      UsersModel.findOne({ _id: this.user })
+      .then((user) => {
+        user.estimates = user.estimates.filter((cust) => {
+          return !cust.id === this.customer;
+        });
+        user.save();
+      });
+    });
   }
 
   removeCustomerfromQueue() {
@@ -82,6 +136,7 @@ class CustomerStatus {
         this.addCustomertoUserList();
         this.removeCustomerfromQueue();
         this.setEstimatorName();
+        this.setCustomerStatus(5);
       } else {
         this.removeCustomerfromQueue();
       }
@@ -141,17 +196,6 @@ class CustomerStatus {
           customer.save();
         });
   }
-  getEstimateStatus(status) {
-    if (status === 0) {
-      return 6;
-    }
-    if (status === 1) {
-      return 7;
-    }
-    if (status === 2) {
-      return 8;
-    }
-  }
 
   setStatusEstimator(status) {
     UsersModel.findOne({ _id: this.user })
@@ -165,26 +209,23 @@ class CustomerStatus {
           })
           user.save();
        });
-      CustomersModel.findOne({ _id: this.customer })
+          CustomersModel.findOne({ _id: this.customer })
         .then((customer) => {
          customer.status = this.getEstimateStatus(status);
           customer.save();
         });
   }
 
-  getSurveyor() {
-    const surveyor = new Promise((resolve, reject) => {
-      UsersModel.findOne({ _id: this.user })
-       .then((user) => {
-         resolve({
-           firstName: user.firstName,
-           lastName: user.lastName,
-           mobile: user.mobile,
-           id: user._id,
-         });
-       });
-    });
-    return surveyor;
+  setEstimatorName() {
+    this.getUser()
+      .then((user) => {
+         CustomersModel.findOne({ _id: this.user })
+           .then((customer) => {
+             customer.estimator = `${user.firstName} ${user.lastName}`;
+             customer.estmatorID = user._id;
+             customer.save();
+           });
+      });
   }
 
   setsurveyType(type) {
@@ -229,6 +270,57 @@ class CustomerStatus {
     this.setSurveyorforCustomer();
     this.setsurveyType(1);
   }
+
+  addCustomertoEstimator() {
+    CustomersModel.findOne({ _id: this.customer })
+       .then((customer) => {
+         UsersModel.findOne({ _id: this.user })
+          .then((user) => {
+            user.estimates.push({
+              id: customer._id,
+              firstName: customer.firstName,
+              lastName: customer.lastName,
+              email1: customer.email1,
+              email2: customer.email2,
+              cphone: customer.cphone,
+              hphone: customer.hphone,
+              wphone: customer.wphone,
+              address: customer.address,
+              status: 0,
+            });
+            user.save();
+          });
+       });
+  }
+ addCustomertoQueue () {
+   this.getCustomer()
+    .then((customer) => {
+      QueueModel.findOne({ customer: customer._id })
+      .then((data) => {
+        if (data === null) {
+          const queueItem = new QueueModel({
+            id: customer._id,
+            firstName: customer.firstName,
+            lastName: customer.lastName,
+            email1: customer.email1,
+            email2: customer.email2,
+            hphone: customer.hphone,
+            cphone: customer.cphone,
+            wphone: customer.wphone,
+            address: customer.address,
+          });
+          queueItem.save().then((res) => {
+            CustomersModel.findOne({ _id: customer._id })
+             .then((customer) => {
+               customer.estimateQueueId = res._id;
+               customer.save();
+             });
+          });
+        }
+      });
+    });
+ }
+
   deleteCustomerfromSurveyor() {
     UsersModel.findOne({ _id: this.user })
       .then((user) => {
@@ -252,7 +344,118 @@ class CustomerStatus {
       });
   }
 
+  checkCustomerStatus() {
+    const CustStatus = new Promise((resolve, reject) => {
+      CustomersModel.findOne({ _id: this.customer }).then((customer) => { resolve(customer.status); });
+    });
+    const SurveyorStatus = new Promise((resolve, reject) => {
+      UsersModel.findOne({ _id: this.user }).then((user) => {
+        user.newCustomers.forEach((customer) => {
+          if (customer.id === this.customer) {
+            resolve(customer.status);
+          }
+        });
+      });
+    });
+    const EstimatorStatus = new Promise((resolve, reject) => {
+      UsersModel.findOne({ _id: this.user }).then((user) => {
+        if (user.estimates.length === 0) {
+          resolve(false);
+        }
+        user.estimates.forEach((customer) => {
+          if (customer.id === this.customer) {
+            resolve(customer.status);
+          }
+          resolve(false);
+        });
+      });
+    });
+    return Promise.all([CustStatus, SurveyorStatus, EstimatorStatus]);
+  }
+
+ removeCustomerfromQueue() {
+   this.getCustomer().then((customer) => {
+         QueueModel.findByIdAndRemove(customer.estimateQueueId)
+          .then(data => console.log(data)).catch(err => console.error(err));
+     });
+ }
+
+checkCustomerinQueue(){
+  const status = new Promise((resolve, reject) => {
+    QueueModel.findOne({ id: this.customer })
+     .then((customer) => {
+       if(customer){
+         resolve(true);
+       }
+       resolve(false);
+     })
+     .catch(() => { resolve(false) });
+  });
+  return status;
 }
 
+
+  setCustomerStatus(status) {
+    CustomersModel.findOne({ _id: this.customer })
+       .then((customer) => {
+         customer.status = status;
+         customer.save();
+       });
+  }
+
+  setSurveyReadyforPricing(status) {
+    CustomersModel.findOne({ _id: this.customer })
+       .then((customer) => {
+         customer.surveyReadyforPrice = status;
+         customer.save();
+       });
+  }
+  sendPushNotifcationtoEstimators() {
+    this.getCustomer()
+       .then((customer) => {
+         const oneSignal = new OneSignalClass(this.user, customer);
+         oneSignal.sendPushtoEstimators();
+       });
+  }
+
+  sendPushToEstimator() {
+    CustomersModel.findOne({ _id: this.customer })
+       .then((customer) => {
+         const oneSignal = new OneSignalClass(null, customer);
+         oneSignal.sendPushtoEstimators();
+       });
+  }
+
+  toogleCustomerReady() {
+    this.checkCustomerStatus()
+      .then((status) => {
+        const customerStatus = {
+          cust: status[0],
+          survey: status[1],
+          est: status[2],
+        };
+        if (customerStatus.cust <= 3 && customerStatus.survey <= 3) {
+          this.setStatusSurveyor(4);
+          this.setSurveyReadyforPricing(true);
+          this.addCustomertoQueue();
+          this.sendPushNotifcationtoEstimators();
+        }
+        if (customerStatus.survey === 4) {
+          this.checkCustomerinQueue()
+          .then((res) => {
+            if (res) {
+              this.removeCustomerfromQueue();
+              this.setSurveyReadyforPricing(false);
+              this.setStatusSurveyor(3);
+            } else {
+              this.deleteCustomerfromEstimator();
+              this.setSurveyReadyforPricing(false);
+              this.setStatusSurveyor(3);
+            }
+          });
+        }
+      });
+  }
+}
 
 export default CustomerStatus;
--- server/helpers/oneSignalClass.js
+++ server/helpers/oneSignalClass.js
@@ -1,10 +1,11 @@
 import UsersModel from '../lib/UserModel';
+import axios from 'axios';
 
 class OneSignalClass {
-  constructor(user) {
+  constructor(user, customer) {
     this.user = user;
+    this.customer = customer;
   }
-
   savePlayerIdtoUser(id) {
     return UsersModel.findOne({ _id: this.user })
      .then((user) => {
@@ -13,7 +14,21 @@ class OneSignalClass {
        return true;
      });
   }
-
+  sendPushtoEstimators() {
+    axios({
+      method: 'post',
+      url: 'https://onesignal.com/api/v1/notifications',
+      data: {
+        app_id: 'ca2c967a-2b86-4315-8a1d-ed5e58b92f95',
+        included_segments: ['Estimators'],
+        data: { customer: this.customer._id },
+        contents: { en: 'New Estimate Ready' },
+        headings: { en: this.customer.address },
+        buttons: [{ id: 'id1', text: 'Accept' }],
+      },
+      headers: { Authorization: `Basic ${process.env.ONE_SIGNAL_AUTH}` },
+    }).then(data => console.log('data')).catch(err => console.error(err));
+  }
 }
 
 export default OneSignalClass;
